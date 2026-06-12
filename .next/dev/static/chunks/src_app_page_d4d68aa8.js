add73c19a7a3dedd4462b720785004120260412d
Giustucci17 <tuccigiuseppe17@gmail.com>
2026-06-12 12:49:38 +0200

    refactor: remove legacy code and update README

--- .next/dev/static/chunks/src_app_page_d4d68aa8.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_dist_02409dcd._.js",
-  "static/chunks/src_app_Home_50e94a0f.css"
-],
-    source: "dynamic"
-});
